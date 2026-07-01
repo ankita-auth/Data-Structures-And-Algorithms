bb7699caddd99b4af7db1763339f4740a937fab9
ankitasingh2004 <ankitasinghofficial9652@gmail.com>
2026-07-01 07:04:42 +0530

    01-07-26

---
+++ Language_Basics/CPP_Basics/pass_by_ref&val.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+// void explainPassByValueAndReference(int x){
+//     x=x+10;
+// }
+// int main(){
+//     int num=5;
+//     explainPassByValueAndReference(num);
+//     cout<<num;
+//     return 0;
+
+// }
+//-------------------------------------X---------------------------------
+// void explainPassByValueAndReference(int &x){
+//     x=x+10;
+// }
+// int main(){
+//     int num=5;
+//     explainPassByValueAndReference(num);
+//     cout<<num;
+//     return 0;
+
+// }
+
+//-----------------------------------------------X-----------------------------------------
+// void explainPassByValueAndReference(int arr[]){
+//     arr[0]=100;
+// }
+// int main(){
+//     int arr[]={6,7,8,1,2};
+//     explainPassByValueAndReference(arr);
+//     cout<<arr[0];
+//     return 0;
+
+// }
+//------------------------------------------------------X-----------------------
+void explainPassByValueAndReference(int *arr){
+    arr[0]=100;
+}
+int main(){
+    int arr[]={6,7,8,1,2};
+    explainPassByValueAndReference(arr);
+    cout<<arr[0];
+    return 0;
+
+}

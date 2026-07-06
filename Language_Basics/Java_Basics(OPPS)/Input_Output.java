542aeb6a78079b0fc326c90f3a13bccc39571157
ankitasingh2004 <ankitasinghofficial9652@gmail.com>
2026-07-06 07:36:18 +0530

    06-07-26

---
+++ Language_Basics/Java_Basics(OPPS)/Input_Output.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Input_Output{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        System.out.println(num);
+        double n=scanner.nextDouble();
+        System.out.println(n);
+
+
+    }
+}
--- Language_Basics/Java_Basics(OPPS)/string.java
+++ Language_Basics/Java_Basics(OPPS)/string.java
@@ -1,10 +1,22 @@
 public class string {
     public static void main(String[] args){
-        String name="Ankita";
-        //System.out.println(name+" has length "+ name.length());
-        System.out.println(name.charAt(3));
-        System.out.println(name.substring(0,4));
-        System.out.println(name.substring(2));
+        // String name="Ankita";
+        // //System.out.println(name+" has length "+ name.length());
+        // System.out.println(name.charAt(3));
+        // System.out.println(name.substring(0,4));
+        // System.out.println(name.substring(2));
+
+        String name="raj";
+        String lastName="vikram";
+        name+=" "+lastName;
+        System.out.println(name);
+        boolean result=name.equals(lastName);
+        System.out.println(result);
+
+        boolean results=name==lastName;
+        System.out.println(results);
+
+
         
     
         
